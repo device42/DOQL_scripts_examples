73b2c4c4c8cfc8337c0f7b4207900bb5f68bb2fa
Jason Goldberg <jason.goldberg@device42.com>
2020-10-12 09:32:18 -0400

    Update all_devices to 16.19

--- Recommended DOQL/Previous Versions/all_devices_pre1619.sql
+++ Recommended DOQL/Previous Versions/all_devices_pre1619.sql
@@ -2,7 +2,7 @@
  - Name: All Devices
  - Purpose: An extensive query to report on all devices and majority of related objects/attributes.
  - Date Created: 10/01/20
- - Changes: 10/12/20 Updated to use new subtypes and view_device_v2 introduced in 16.19
+ - Changes:
 */
     select
         d.last_edited "Last_Discovered",
@@ -10,16 +10,16 @@
         d.in_service "In Service",
         d.service_level "Service_Level",
         d.type "Device_Type",
-        COALESCE(d.physicalsubtype, '') || COALESCE(d.virtualsubtype, '') "Device Subtype",
+        d.sub_type "Device Subtype",
+        d.virtual_subtype "Virtual_Subtype",
         d.serial_no "Device_Serial",
         d.virtual_host "Virtual Host",
         d.network_device "Network Device",
-        d.os_architecture "OS Architecture",
-        d.total_cpus "Total CPUs",
-        d.core_per_cpu "Cores Per CPU",
-        d.threads_per_core "Threads Per Core",
-        d.cpu_speed "CPU Speed",
-        d.total_cpus*d.core_per_cpu "Total Cores",
+        d.os_arch "OS_Arch",
+        d.cpucount "CPU Sockets",
+        d.cpucore "Cores Per CPU",
+        d.cpupower "CPU Speed",
+        d.cpucount*d.cpucore "Total Cores",
         d.ram "RAM",
         v2.name "OS Vendor",
         osc.category_name "OS Category",
@@ -61,15 +61,17 @@
                   from view_mountpoint_v1 mp
                   where mp.device_fk = d.device_pk),
                   ' | ')) mount_points,
+        d.cloud_vendor_fk,
+        ci.vendor_fk,
         ci.account,
         cv.name "Cloud Service Provider",
         ci.service_name "Cloud Service Name",
-        ci.instance_id "Cloud Instance ID",
+        d.cloud_instance_id "Cloud Instance ID",
         ci.instance_name "Cloud Instance Name",
         ci.instance_type "Cloud Instance Type",
-        ci.status "Cloud Instance Status",
-        ci.location "Cloud Location",
-        ci.notes "Cloud Notes",
+        d.cloud_status "Cloud Instance Status",
+        d.cloud_location "Cloud Location",
+        d.cloud_notes "Cloud Notes",
         pch.po_date "PO Date",
         pch.cost "PO Cost",
         pli.cost "Line Item Cost",
@@ -89,14 +91,14 @@
        round(((select sum(m.capacity-m.free_capacity)/1024 from view_mountpoint_v1 m where m.device_fk = d.device_pk and m.fstype_name <> 'nfs' and m.fstype_name <> 'nfs4' and m.filesystem not like '\\\\%')), 2)"Used Space",
        round(((select sum(m.capacity/1024) from view_mountpoint_v1 m where m.device_fk = d.device_pk and m.fstype_name <> 'nfs' and m.fstype_name <> 'nfs4' and m.filesystem not like '\\\\%')), 2)"Total Space",
        round(((select sum(m.free_capacity/1024) from view_mountpoint_v1 m where m.device_fk = d.device_pk and m.fstype_name <> 'nfs' and m.fstype_name <> 'nfs4' and m.filesystem not like '\\\\%')), 2)"Total Free Space"
-        from view_device_v2 d
+        from view_device_v1 d
         left join view_purchaselineitems_to_devices_v1 ptd on ptd.device_fk = d.device_pk
         left join view_purchaselineitem_v1 pli on ptd.purchaselineitem_fk = pli.purchaselineitem_pk
         left join view_purchase_v1 pch on pch.purchase_pk = pli.purchase_fk
         left join view_oseoleos_v1 ose on ose.os_fk = d.os_fk
         left join view_hardware_v1 h on d.hardware_fk = h.hardware_pk
         left join view_vendor_v1 v on h.vendor_fk = v.vendor_pk
-        /*left join view_vendor_v1 cv on d.cloud_vendor_fk = cv.vendor_pk */ --- old for view_device_v1
+        left join view_vendor_v1 cv on d.cloud_vendor_fk = cv.vendor_pk
         left join view_room_v1 sr on sr.room_pk = d.storage_room_fk 
         left join view_rack_v1 r on d.rack_fk = r.rack_pk
         left join view_room_v1 m on r.room_fk = m.room_pk
@@ -105,5 +107,4 @@
         left join view_vendor_v1 v2 on osc.vendor_fk = v2.vendor_pk
         left join view_device_custom_fields_flat_v1 dcf on dcf.device_fk = d.device_pk
         left join view_cloudinstance_v1 ci on ci.device_fk = d.device_pk
-        left join view_vendor_v1 cv on cv.vendor_pk = ci.vendor_fk
         order by d.name ASC
