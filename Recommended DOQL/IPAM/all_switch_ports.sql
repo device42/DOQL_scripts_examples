fb4e8a4261c565755ef6185170eff8a746651f8d
Jason Goldberg <jason.goldberg@device42.com>
2021-02-05 08:05:22 -0500

    add all switch ports sql and update app comp and software

--- Recommended DOQL/IPAM/all_switch_ports.sql
+++ Recommended DOQL/IPAM/all_switch_ports.sql
@@ -10,7 +10,7 @@ select
     v.number "VLAN"
 FROM
     view_netport_v1 sp
-    join view_device_v2 sd on sd.device_pk = sp.device_fk and s.network_device = 't'
+    join view_device_v2 sd on sd.device_pk = sp.device_fk and sd.network_device = 't'
     left join view_netport_v1 rp on rp.remote_netport_fk = sp.remote_netport_fk
     left join view_device_v2 rd on rd.device_pk = rp.device_fk
     left join view_vlan_on_netport_v1 vp on vp.netport_fk = sp.netport_pk
