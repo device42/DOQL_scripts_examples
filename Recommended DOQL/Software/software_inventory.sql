ee36a85e4503b912ccea31432fb29f4c68aa8713
Jason Goldberg <jason.goldberg@device42.com>
2021-04-07 12:38:57 -0400

    Updated Software Inventory

--- Recommended DOQL/Software/software_inventory.sql
+++ Recommended DOQL/Software/software_inventory.sql
@@ -1,18 +1,25 @@
 /* Software Inventory Data Extract    
-   
-   Update 2020-10-19
-   - updated the view_device_v1 to view_device_v2	
+    Update 2020-10-19 
+        - updated the view_device_v1 to view_device_v2
+    Update 2021-04-07
+        - changed array_to_string to CTE to fix duplicate SIU records when devices have more than 1 IP
+        - changed left join on device view to join to remove siu records where there is no device fk
 */
+WITH ip_info AS (
+    SELECT 
+        ip.device_fk d_fk,
+        array_to_string(array_agg(ip.ip_address), ' | ') d_ips
+    FROM 
+        view_ipaddress_v1 ip 
+    WHERE ip.device_fk IS NOT NULL
+    GROUP BY ip.device_fk
+)
 Select 
 	d.name "Device Name"
-	,(SELECT array_to_string(array(
-			Select ip.ip_address
-			From view_ipaddress_v1 ip
-			Where ip.device_fk = d.device_pk),
-			' | ')) "Device IP Addresses"
+    ,i.d_ips "Device IP Addresses"
 	,s.software_pk "Software ID"
+    ,siu.softwareinuse_pk "SIU PK"
 	,s.name "Software Name"
-    ,v.name "Vendor"
 	,siu.component_name "Software Component Name"
 	,ac.name "Related App Comp"
 	,siu.alias_name "Software Alias"
@@ -22,8 +29,7 @@ Select
 	,siu.install_path "Installation Path"
 From view_softwareinuse_v1 siu
 Left Join view_software_v1 s ON s.software_pk = siu.software_fk
-Left Join view_vendor_v1 v ON v.vendor_pk = s.vendor_fk
 Left Join view_appcomp_v1 ac ON siu.appcomp_fk = ac.appcomp_pk
-Left Join view_device_v2 d ON siu.device_fk = d.device_pk
-Left Join view_ipaddress_v1 ip ON ip.device_fk = d.device_pk
+Join view_device_v2 d ON siu.device_fk = d.device_pk
+Left Join ip_info i ON i.d_fk = d.device_pk
 Order by d.name ASC
