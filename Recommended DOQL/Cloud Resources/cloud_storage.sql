196f68bfdf72d2f8e47d129a50bb5b50433b9945
Jason Goldberg <jason.goldberg@device42.com>
2021-04-15 16:48:23 -0400

    Update cloud_storage.sql

--- Recommended DOQL/Cloud Resources/cloud_storage.sql
+++ Recommended DOQL/Cloud Resources/cloud_storage.sql
@@ -2,9 +2,10 @@
  - Name: cloud storage
  - Purpose: provides details on discovered Cloud storage services.
  - Date Created: 10/01/20
- - Changes:
+ - Updated - 4/15/21
+    - Change view_device_v1 to view_device_v2
 */
-Select 
+Select DISTINCT
     ci.cloudinfrastructure_name "Cloud Infrastructure Name"
     ,ci.account_id "Account ID"
     ,ci.organization "Organization"
@@ -12,10 +13,11 @@ Select
     ,r2.resource_name "Resource Name"
     ,r2.identifier "Resource ID"
     ,unnest(r2.category) "Resource Category"
-    ,r2.subtype "Resource Subtype"
+    ,r2.vendor_resource_subtype "Resource Subtype"
     ,r2.region "Resource Region"
     ,unnest(r2.zones) "Resource Zones"
-    ,r2.vendor_define_type "Type"
+    ,r2.vendor_resource_type "Type"
 From view_resource_v2 r2 
 Left Join view_cloudinfrastructure_v2 ci On ci.cloudinfrastructure_pk = r2.cloudinfrastructure_fk
-Left Join view_device_v1 d on d.device_pk = r2.device_fk
+Left Join view_deviceresource_v1 dr On dr.resource_fk = r2.resource_pk
+Left Join view_device_v2 d On d.device_pk = dr.device_fk
