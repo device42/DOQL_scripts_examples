8e87dd913e2ea570bd779fd59fdfafc49d9ec716
Jason Goldberg <jason.goldberg@device42.com>
2021-04-15 13:46:49 -0400

    Update 5. Service Connections, Dev and Prod Devices.sql

--- Recommended DOQL/Compliance and Audit/5. Service Connections, Dev and Prod Devices.sql
+++ Recommended DOQL/Compliance and Audit/5. Service Connections, Dev and Prod Devices.sql
@@ -1,10 +1,9 @@
 with ips as
-    
     (select distinct
                 ld.device_pk as "Listening Device PK"
                 ,ld.name as "Listening Device Name"
                 ,cd.device_pk as "Client Device PK"
-                ,ld.name as "Client Device Name"
+                ,cd.name as "Client Device Name"
                 ,coalesce(ld.service_level, '') as "Listening Device Service Level"
                 ,coalesce(cd.service_level, '') as "Client Device Service Level"
                 ,case when (ld.service_level like 'Production' and cd.service_level not like 'Production')
@@ -14,7 +13,6 @@ with ips as
             left Join view_device_v2 ld ON ld.device_pk = sc.listener_device_fk
             Left Join view_device_v2 cd ON cd.device_pk = sc.client_device_fk
             Where sc.client_ip != '127.0.0.1' and sc.client_ip != '::1')
-            
   select *
   from ips
   where "Is Dev/Prod Mismatch" like 'Yes'
