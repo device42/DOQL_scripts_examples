cfe3559d36c192c4439aee0d2abaa8296a457a58
Jason Goldberg <jason.goldberg@device42.com>
2021-12-13 14:49:39 -0500

    Add log4j doql

---
+++ Recommended DOQL/Compliance and Audit/Service Instances and Application Components Including Log4j Path.sql
@@ -0,0 +1,19 @@
+/* 0-day query
+*/
+
+With
+serviceinstance_data as (
+Select * From view_serviceinstance_cmdpaths_v2 sicp
+Where  position('log4j' IN lower(sicp.cmd_path)) > 0
+)
+/* Report out */
+
+Select  Distinct vnd.name "Vendor", svc.displayname "Service", svc.pretty_name "Pretty Name", sw.name "Software", siu.version "Version", siu.alias_name "Alias", dev.name "Host", sid.cmd_path "CMD Path", si.first_detected "First Detected", si.last_updated "Last Updated"
+	From serviceinstance_data sid
+       Left Join view_serviceinstance_v2 si ON si.serviceinstance_pk = sid.serviceinstance_fk
+	   Left Join view_device_v2 dev ON dev.device_pk = si.device_fk
+	   Left Join view_service_v2 svc ON svc.service_pk = si.service_fk
+	   Left Join view_softwareinuse_v1 siu ON siu.softwareinuse_pk = si.softwareinuse_fk
+       Left Join view_software_v1 sw ON sw.software_pk = siu.software_fk	   
+	   Left Join view_vendor_v1 vnd ON vnd.vendor_pk = svc.vendor_fk
+  Order by 1, 2, 3, 4, 5
