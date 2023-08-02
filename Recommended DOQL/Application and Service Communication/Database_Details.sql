b3ef57166e1d1c1b546af76bf2bbf53d085bc61e
M6thrXeat <57998659+M6thrXeat@users.noreply.github.com>
2023-08-02 13:44:09 -0400

    Update Database_Details.sql

--- Recommended DOQL/Application and Service Communication/Database_Details.sql
+++ Recommended DOQL/Application and Service Communication/Database_Details.sql
@@ -1,5 +1,7 @@
 /* Database Details  - Information Extract */
 /* Inline view of Target CTE (inline views) to streamline the process  - 
+   Update 2023-08-01 
+	- updated to use view_appcomp_resources_v2
    Update 2020-10-19
    - updated the view_device_v1 to view_device_v2			 
 		*/
@@ -23,5 +25,6 @@ From view_databasesize_v2 dbs
 JOIN view_database_to_databasesize_v2 dbds ON dbds.databasesize_fk = dbs.databasesize_pk 
 JOIN view_database_v2 db ON dbds.database_fk = db.database_pk
 JOIN view_databaseinstance_v2 di ON db.databaseinstance_fk = di.databaseinstance_pk
-Left Join view_appcomp_v1 ac ON di.appcomp_fk = ac.appcomp_pk
+JOIN view_appcomp_resources_v2 acr ON acr.appcomp_fk = di.appcomp_fk 
+Left Join view_appcomp_v1 ac ON acr.appcomp_fk = ac.appcomp_pk
 Left Join view_device_v2 d ON d.device_pk = ac.device_fk
